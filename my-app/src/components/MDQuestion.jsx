57bc3968f6faa7da21b5b8999119c59d95536bc8
Jerron Lim <jerronlim@Jerrons-MacBook-Pro.local>
2020-04-25 18:19:45 +0800

    Basic MDQuestion

--- my-app/src/components/MDQuestion.jsx
+++ my-app/src/components/MDQuestion.jsx
@@ -1,15 +1,101 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from "react";
+import styled from "styled-components";
+import { Dropdown, DropdownButton } from "react-bootstrap";
+import MyDropdownButton from "./MyDropdownButton";
+import MyDropdownButon from "./MyDropdownButton";
 
-class MDQuestion extends Component {
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
-  render() {
-    return (
-      <div>
-        Question
-      </div>
-    );
-  }
+const StyledQnContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: 2rem;
+`;
 
-}
+const MDQuestion = () => {
+  const mealTypes = [
+    "main course",
+    "side dish",
+    "dessert",
+    "appetizer",
+    "salad",
+    "bread",
+    "breakfast",
+    "soup",
+    "beverage",
+    "sauce",
+    "marinade",
+    "fingerfood",
+    "snack",
+    "drink",
+  ];
+
+  const diffTypes = ["Easy", "Medium", "Challenging"];
+
+  const cuisineTypes = [
+    "African",
+    "American",
+    "British",
+    "Cajun",
+    "Caribbean",
+    "Chinese",
+    "Eastern European",
+    "European",
+    "French",
+    "German",
+    "Greek",
+    "Indian",
+    "Irish",
+    "Italian",
+    "Japanese",
+    "Jewish",
+    "Korean",
+    "Latin American",
+    "Mediterranean",
+    "Mexican",
+    "Middle Eastern",
+    "Nordic",
+    "Southern",
+    "Spanish",
+    "Thai",
+    "Vietnamese",
+  ];
+
+  const [mealType, setMealType] = useState(null);
+  const [difficulty, setDifficulty] = useState(null);
+  const [cuisine, setCuisine] = useState(null);
+
+  return (
+    <StyledContainer>
+      <h3>What kind of recipe are you looking for?</h3>
+      <StyledQnContainer>
+        <MyDropdownButon
+          title="Meal type"
+          optionList={mealTypes}
+          setOption={setMealType}
+          option={mealType}
+        />
+        <MyDropdownButon
+          title="Difficulty"
+          optionList={diffTypes}
+          setOption={setDifficulty}
+          option={difficulty}
+        />
+        <MyDropdownButon
+          title="Cuisine"
+          optionList={cuisineTypes}
+          setOption={setCuisine}
+          option={cuisine}
+        />
+      </StyledQnContainer>
+    </StyledContainer>
+  );
+};
 
 export default MDQuestion;

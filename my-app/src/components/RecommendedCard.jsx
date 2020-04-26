28e805ced587a15ad7b1cab7379e3ab34521aead
Jerron Lim <jerronlim@Jerrons-MacBook-Pro.local>
2020-04-27 01:15:33 +0800

    Make answer card more mobile friendly

--- my-app/src/components/RecommendedCard.jsx
+++ my-app/src/components/RecommendedCard.jsx
@@ -1,47 +1,61 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import { Row, Col, Card, Button } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 import Recipe from "./Recipe";
 import fakeRecipe from "../data/recipe.json";
 
 const SCard = styled(Card)`
-  max-width: 1016px;
+  max-width: 600px;
   margin: auto;
   border-radius: 20px;
   overflow: hidden;
+  margin-bottom: 1rem;
 `;
 
 const SCardTitle = styled.h4`
   font-weight: 700;
-  padding-top: 15px;
+  padding-top: 0.5rem;
 `;
 
-const SCardText = styled.div`
-  padding: 20px;
-`;
+// const SCardText = styled.div`
+//   padding: 20px;
+// `;
 
-const NavArea = styled.div`
-  padding: 20px;
-`;
+// const NavArea = styled.div`
+//   padding: 20px;
+// `;
 
-const Image = styled.img`
-  width: 100%;
-  height: 700px;
-  background: ${(props) =>
-    props.recipe &&
-    `url(${JSON.stringify(props.recipe.image)})
-    no-repeat center`};
-  background-size: cover;
-  padding: -15px;
-`;
+// const Image = styled.img`
+//   width: 100%;
+//   height: 700px;
+//   background: ${(props) =>
+//     props.recipe &&
+//     `url(${JSON.stringify(props.recipe.image)})
+//     no-repeat center`};
+//   background-size: cover;
+//   padding: -15px;
+// `;
 
-const ImgCol = styled(Col)`
-  padding-right: 0px;
-`;
+// const ImgCol = styled(Col)`
+//   padding-right: 0px;
+// `;
 
 const Link = styled.a`
   text-decoration: underline;
   color: black;
+`;
+
+const StyledCardImg = styled(Card.Img)`
+  width: 100%;
+  height: 20rem;
+  object-fit: cover;
+  background: no-repeat top;
+`;
+
+const StyledCardBody = styled(Card.Body)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
 
 class RecommendedCard extends Component {
@@ -70,31 +84,28 @@ class RecommendedCard extends Component {
     const recipe = this.state.recipe;
 
     return (
-      <SCard>
-        <Row>
-          <ImgCol>
-            <Image recipe={recipe}></Image>
-          </ImgCol>
-          <Col>
-            <SCardText>
-              <NavArea></NavArea>
-              <Link href="www.kitchen.com/recipe" target="_blank">
-                www.kitchen.com
-              </Link>
-              <SCardTitle>{recipe.title}</SCardTitle>
+      <>
+        <SCard>
+          <StyledCardImg variant="top" src={recipe.image} />
+          <StyledCardBody>
+            <SCardTitle>{recipe.title}</SCardTitle>
+            <Link href="www.kitchen.com/recipe" target="_blank">
+              www.kitchen.com
+            </Link>
+            <Card.Text>
               <Recipe
                 key={recipe.id}
                 id={recipe.id}
                 recipeIngredients={recipe.extendedIngredients}
                 recipeMethod={recipe.analyzedInstructions}
               />
-              <Button variant="primary" href={recipe.sourceUrl} target="_blank">
-                Go to site
-              </Button>
-            </SCardText>
-          </Col>
-        </Row>
-      </SCard>
+            </Card.Text>
+            <Button variant="primary" href={recipe.sourceUrl} target="_blank">
+              Go to site
+            </Button>
+          </StyledCardBody>
+        </SCard>
+      </>
     );
   }
 }

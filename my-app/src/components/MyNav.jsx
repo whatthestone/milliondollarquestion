db0b6c605b64e77f0d384bb545d1abbb6327f2e4
Jerron Lim <l.y.z.jerron@gmail.com>
2020-04-26 11:59:50 +0800

    Fixed Navbar (#2)
    * Fix Navbar
    
    * Change file name for Nav to MyNav
    
    Co-authored-by: Jerron Lim <jerronlim@Jerrons-MacBook-Pro.local>

---
+++ my-app/src/components/MyNav.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import { Navbar, NavDropdown, Nav } from "react-bootstrap";
+
+const MyNav = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="/">Million Dollar Question</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/profile">Profile</Nav.Link>
+          <Nav.Link href="/main">Main</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default MyNav;

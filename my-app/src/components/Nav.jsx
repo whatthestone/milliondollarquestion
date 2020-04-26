2eecc232cda0f774466e64409c9045a26e8563d1
Jerron Lim <jerronlim@Jerrons-MacBook-Pro.local>
2020-04-26 10:25:19 +0800

    Fix Navbar

--- my-app/src/components/Nav.jsx
+++ my-app/src/components/Nav.jsx
@@ -1,43 +1,21 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-import { Navbar, NavDropdown } from "react-bootstrap";
+import { Navbar, NavDropdown, Nav } from "react-bootstrap";
 
-const Nav = () => {
+const MyNav = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="#home">Million Dollar Question</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Link to="/">Dashboard</Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Link to="/main">Main</Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-          <Link to="/profile">Profile</Link>
-          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/profile">Profile</Nav.Link>
+          <Nav.Link href="/main">Main</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
-
-    //
-    // <ul>
-    //   <li>
-    //     <Link to="/">Dashboard</Link>
-    //   </li>
-    //   <li>
-    //     <Link to="/profile">Profile</Link>
-    //   </li>
-    //   <li>
-    //     <Link to="/main">Main</Link>
-    //   </li>
-    // </ul>
   );
 };
 
-export default Nav;
+export default MyNav;

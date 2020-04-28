e311867bdf5bbde746b5647081bf3e35e8dd33a0
Jerron Lim <jerronlim@Jerrons-MacBook-Pro.local>
2020-04-28 14:48:41 +0800

    Fix fakerecipe overwriting cache recipe on start, reduce dependency on recipeid, reduce number of fetch, move setshowanswer to main

--- my-app/src/components/MDAnswer.jsx
+++ my-app/src/components/MDAnswer.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "styled-components";
 import { Row, Container, Button } from "react-bootstrap";
 import RecommendedCard from "../components/RecommendedCard";

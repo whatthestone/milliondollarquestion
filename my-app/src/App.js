db8bb74787ea805b8ae73850c46a0493114cd5fb
Kai Sing Ng <kaising@Kais-MacBook-Pro-3.local>
2020-04-28 19:03:25 -0700

    save page
    shows saved items using localstorage.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,6 +11,7 @@ import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
 import Main from "./pages/Main";
 import MyNav from "./components/MyNav";
+import Saved from "./pages/Saved";
 
 class App extends Component {
   render() {
@@ -28,6 +29,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Dashboard}></Route>
             <Route path="/pantry" component={Profile}></Route>
+            <Route path="/saved" component={Saved}></Route>
             <Route path="/main" component={MainRouter}></Route>
           </Switch>
         </div>

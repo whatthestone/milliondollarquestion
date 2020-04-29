661a562ada1245c06e85cf52a4e5eb603808fc98
Kai Sing Ng <kaising@Kais-MacBook-Pro-3.local>
2020-04-28 23:52:23 -0700

    cleaning up the design for saved recipes

--- my-app/src/pages/Saved.jsx
+++ my-app/src/pages/Saved.jsx
@@ -1,5 +1,44 @@
 import React, { useState } from "react";
-import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
+import styled from "styled-components";
+import {
+  Container,
+  Card,
+  CardColumns,
+  ListGroup,
+  ListGroupItem,
+} from "react-bootstrap";
+
+const SCardColumns = styled(CardColumns)`
+  padding-top: 50px;
+
+  @media (min-width: 996px) and (max-width: 1200px) {
+    column-count: 4;
+  }
+
+  @media (min-width: 1200px) {
+    column-count: 5;
+  }
+`
+
+const Header = styled.h3`
+  padding-top: 30px;
+  font-weight: 700 !important;
+  text-align: center;
+`
+
+const SCardText = styled.div`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 5;
+  -webkit-box-orient: vertical;
+`
+
+const Subheader = styled.div`
+  font-weight: 500;
+  padding-top: 5px;
+  padding-bottom: 5px;
+`
 
 const Saved = ({}) => {
   const [savedRecipes] = useState(
@@ -7,21 +46,27 @@ const Saved = ({}) => {
   );
 
   return (
-    <div>
-      {savedRecipes &&
-        savedRecipes.map((recipe, key) => (
-          <Card key={key} style={{ width: "18rem", margin: "0.5rem" }}>
-            <Card.Img variant="top" src={recipe.image} />
-            <Card.Body>
-              <Card.Title>{recipe.title}</Card.Title>
-              <Card.Text style={{ height: "100px", overflow: "hidden"}}>
-                {recipe.extendedIngredients &&
-                  recipe.extendedIngredients.map(ingredient => (`${ingredient.name},`))}
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        ))}
-    </div>
+    <Container>
+      <Header>Saved Recipes</Header>
+      <SCardColumns>
+        {savedRecipes &&
+          savedRecipes.map((recipe, key) => (
+            <Card key={key}>
+              <Card.Img variant="top" src={recipe.image} />
+              <Card.Body>
+                <Card.Title>{recipe.title}</Card.Title>
+                <SCardText style={{ maxLines: "5" }}>
+                  <Subheader>Ingredients: </Subheader>
+                  {recipe.extendedIngredients &&
+                    recipe.extendedIngredients.map(
+                      (ingredient, index) => index ? `, ${ingredient.name}` : `${ingredient.name}`
+                    )}
+                </SCardText>
+              </Card.Body>
+            </Card>
+          ))}
+      </SCardColumns>
+    </Container>
   );
 };
 

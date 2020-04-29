44769a0951d05c910ed2c32c42f71450cf593427
Kai Sing Ng <kaising@Kais-MacBook-Pro-3.local>
2020-04-29 00:19:23 -0700

    responsive styling for saved items

--- my-app/src/pages/Saved.jsx
+++ my-app/src/pages/Saved.jsx
@@ -1,29 +1,53 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import mediaHelper from "styled-media-helper";
 import {
   Container,
   Card,
   CardColumns,
   ListGroup,
   ListGroupItem,
+  Button
 } from "react-bootstrap";
+
+
+const media = mediaHelper({
+  sm: 320,
+  md: 768,
+  lg: 992,
+  xl: 1240
+});
 
 const SCardColumns = styled(CardColumns)`
   padding-top: 50px;
 
-  @media (min-width: 996px) and (max-width: 1200px) {
+  ${media.only('sm')} {
+    column-count: 2;
+    font-size: 12px;
+  }
+
+  ${media.only('md')} {
+    column-count: 3;
+  }
+
+  ${media.only('lg')} {
     column-count: 4;
   }
 
-  @media (min-width: 1200px) {
+  ${media.only('xl')} {
     column-count: 5;
   }
 `
 
-const Header = styled.h3`
+const SHeader = styled.h3`
   padding-top: 30px;
   font-weight: 700 !important;
   text-align: center;
+
+  ${media.only('sm')} {
+    font-size: 20px;
+  }
+
 `
 
 const SCardText = styled.div`
@@ -34,10 +58,21 @@ const SCardText = styled.div`
   -webkit-box-orient: vertical;
 `
 
+const SCardTitle = styled(Card.Title)`
+
+  ${media.only('sm')} {
+    font-size: 16px;
+  }
+`
+
 const Subheader = styled.div`
   font-weight: 500;
   padding-top: 5px;
   padding-bottom: 5px;
+
+  ${media.only('sm')} {
+    font-size: 14px;
+  }
 `
 
 const Saved = ({}) => {
@@ -47,14 +82,14 @@ const Saved = ({}) => {
 
   return (
     <Container>
-      <Header>Saved Recipes</Header>
+      <SHeader>Saved Recipes</SHeader>
       <SCardColumns>
         {savedRecipes &&
           savedRecipes.map((recipe, key) => (
             <Card key={key}>
               <Card.Img variant="top" src={recipe.image} />
               <Card.Body>
-                <Card.Title>{recipe.title}</Card.Title>
+                <SCardTitle>{recipe.title}</SCardTitle>
                 <SCardText style={{ maxLines: "5" }}>
                   <Subheader>Ingredients: </Subheader>
                   {recipe.extendedIngredients &&
@@ -62,6 +97,7 @@ const Saved = ({}) => {
                       (ingredient, index) => index ? `, ${ingredient.name}` : `${ingredient.name}`
                     )}
                 </SCardText>
+                <Button style={{ marginTop: "10px"}} size="sm" block href={recipe.sourceUrl}>See recipe</Button>
               </Card.Body>
             </Card>
           ))}

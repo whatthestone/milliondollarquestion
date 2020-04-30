dbb121e7429372576c648c2a7d0cdabf6303bd15
Kai Sing Ng <kaising@Kais-MacBook-Pro-3.local>
2020-04-29 18:14:21 -0700

    delete and undo function for saved items lists
    - Deleting a card will remove from localstorage and update savedRecipe State
    - Undoing will allow last card deleted to come back and be restored into localstore. Deleted recipes are stored as a component's state
    - Delete button appears on hover over each card.

--- my-app/src/pages/Saved.jsx
+++ my-app/src/pages/Saved.jsx
@@ -1,103 +1,83 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import mediaHelper from "styled-media-helper";
-import {
-  Container,
-  Card,
-  CardColumns,
-  Button
-} from "react-bootstrap";
-
+import { Container, Card, CardColumns, Button } from "react-bootstrap";
+import SavedCard from "../components/SavedCard";
 
 const media = mediaHelper({
   sm: 320,
   md: 768,
   lg: 992,
-  xl: 1200
+  xl: 1200,
 });
 
 const SCardColumns = styled(CardColumns)`
   padding-top: 50px;
 
-  ${media.only('sm')} {
+  ${media.only("sm")} {
     column-count: 2;
     font-size: 12px;
   }
 
-  ${media.only('md')} {
+  ${media.only("md")} {
     column-count: 3;
   }
 
-  ${media.only('lg')} {
+  ${media.only("lg")} {
     column-count: 4;
   }
 
-  ${media.only('xl')} {
+  ${media.only("xl")} {
     column-count: 5;
   }
-`
+`;
 
 const SHeader = styled.h3`
   padding-top: 30px;
   font-weight: 700 !important;
   text-align: center;
 
-  ${media.only('sm')} {
+  ${media.only("sm")} {
     font-size: 20px;
   }
-
-`
-
-const SCardText = styled.div`
-  overflow: hidden;
-  text-overflow: ellipsis;
-  display: -webkit-box;
-  -webkit-line-clamp: 5;
-  -webkit-box-orient: vertical;
-`
-
-const SCardTitle = styled(Card.Title)`
-
-  ${media.only('sm')} {
-    font-size: 16px;
-  }
-`
-
-const Subheader = styled.div`
-  font-weight: 500;
-  padding-top: 5px;
-  padding-bottom: 5px;
-
-  ${media.only('sm')} {
-    font-size: 14px;
-  }
-`
+`;
 
 const Saved = () => {
-  const [savedRecipes] = useState(
+  const [savedRecipes, setSavedRecipes] = useState(
     JSON.parse(localStorage.getItem("savedRecipes"))
   );
+
+  const [deletedRecipes, setDeletedRecipes] = useState(null);
+
+  const handleDelete = (recipe) => {
+    console.log(recipe);
+    let savedRecipes = [];
+    if (localStorage.getItem("savedRecipes")) {
+      setDeletedRecipes(localStorage.getItem("savedRecipes"));
+      const savedRecipes = JSON.parse(localStorage.getItem("savedRecipes"));
+      let filteredRecipes = savedRecipes.filter(
+        (item) => item.id !== recipe.id
+      );
+      console.log(filteredRecipes);
+      localStorage.setItem("savedRecipes", JSON.stringify(filteredRecipes));
+      setSavedRecipes(filteredRecipes);
+    }
+  };
+
+  const handleUndo = () => {
+    localStorage.setItem("savedRecipes", deletedRecipes);
+    setSavedRecipes(JSON.parse(deletedRecipes));
+    setDeletedRecipes(null);
+  };
 
   return (
     <Container>
       <SHeader>Saved Recipes</SHeader>
+      {deletedRecipes && <Button onClick={handleUndo}>Undo</Button>}
       <SCardColumns>
         {savedRecipes &&
           savedRecipes.map((recipe, key) => (
-            <Card key={key}>
-              <Card.Img variant="top" src={recipe.image} />
-              <Card.Body>
-                <SCardTitle>{recipe.title}</SCardTitle>
-                <SCardText style={{ maxLines: "5" }}>
-                  <Subheader>Ingredients: </Subheader>
-                  {recipe.extendedIngredients &&
-                    recipe.extendedIngredients.map(
-                      (ingredient, index) => index ? `, ${ingredient.name}` : `${ingredient.name}`
-                    )}
-                </SCardText>
-                <Button style={{ marginTop: "10px"}} size="sm" block href={recipe.sourceUrl}>See recipe</Button>
-              </Card.Body>
-            </Card>
+            <SavedCard key={key} recipe={recipe} onDelete={handleDelete} />
           ))}
       </SCardColumns>
     </Container>
